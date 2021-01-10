8c40423ad74ef03c1c8163b97a635dacc699c964
skiffua <oleh_mushora@ukr.net>
2021-01-10 18:49:15 +0200

    Heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VuePictureSwipe from 'vue-picture-swipe'
-import LightGallery from 'vue-light-gallery'
 import router from './router'
 import store from './store'
-import lightbox from 'vlightbox'
 
-Vue.use(lightbox)
-Vue.use(LightGallery)
 Vue.component('vue-picture-swipe', VuePictureSwipe)
 
 Vue.config.productionTip = false
