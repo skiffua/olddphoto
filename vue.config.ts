be91226cfccfd209d9a60657681d71cffcc53267
skiffua <oleh_mushora@ukr.net>
2021-01-26 22:52:41 +0200

    Add prerender plugin

--- vue.config.ts
+++ vue.config.ts
@@ -1,4 +1,22 @@
 module.exports = {
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true,
+      postProcess: route => {
+        // Defer scripts and tell Vue it's been server rendered to trigger hydration
+        route.html = route.html
+          .replace(/<script (.*?)>/g, '<script $1 defer>')
+          .replace('id="app"', 'id="app" data-server-rendered="true"')
+        return route
+      }
+    }
+  },
   module: {
     rules: [
       {
