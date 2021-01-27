334718f4c9530612759496c82ee823ca96411158
skiffua <oleh_mushora@ukr.net>
2021-01-27 16:23:01 +0200

    add spa plugin

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: ['/']
+      })
+    ]
+  }
+}
