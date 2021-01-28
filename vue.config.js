b749c13971b4bda50e40267c033db3548a888a7a
skiffua <oleh_mushora@ukr.net>
2021-01-28 17:38:40 +0200

    add sitemap

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+require = require('esm')(module)
+// const { routes } = require('./src/router/index.js');
+
+module.exports = {
+  pluginOptions: {
+    sitemap: {
+      urls: [
+        'https://dobromyl-historical-photos.herokuapp.com/'
+      ]
+    }
+  }
+}
