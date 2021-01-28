14874c0258333eb6b3c1be6e6d937bedcb237f3f
skiffua <oleh_mushora@ukr.net>
2021-01-28 11:58:38 +0200

    add test3

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const productionPlugins = [
   new PrerenderSPAPlugin({
     staticDir: path.join(__dirname, 'dist'),
-    routes: ['/', '/documentation'],
+    routes: [],
     renderer: new PrerenderSPAPlugin.PuppeteerRenderer({
       // We need to inject a value so we're able to
       // detect if the page is currently pre-rendered.
