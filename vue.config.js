01e3794a2a0cb84b93ceb784eba412d347854968
skiffua <oleh_mushora@ukr.net>
2021-01-28 11:28:12 +0200

    add another

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,21 @@ module.exports = {
         // Required - The path to the webpack-outputted app to prerender.
         staticDir: path.join(__dirname, 'dist'),
         // Required - Routes to render.
-        routes: []
+        routes: ['/'],
+        postProcess (renderedRoute) {
+          renderedRoute.html = renderedRoute.html.replace(/(\<head\>.*?)(\<script.*?\<\/script\>){1,}(.*\<\/head\>)/g, '$1$3');
+          return renderedRoute
+        }
+        // renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+        //   // We need to inject a value so we're able to
+        //   // detect if the page is currently pre-rendered.
+        //   inject: {},
+        //   // Our view component is rendered after the API
+        //   // request has fetched all the necessary data,
+        //   // so we create a snapshot of the page after the
+        //   // `data-view` attribute exists in the DOM.
+        //   //renderAfterElementExists: '[data-view]',
+        // }),
       })
     ]
   }
