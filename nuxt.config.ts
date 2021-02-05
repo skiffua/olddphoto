cf7dbab4a5f8a7088f01feb720c20cdfd3c09e81
skiffua <oleh_mushora@ukr.net>
2021-02-05 22:33:47 +0200

    improve seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Добромиль, історичні фото. Dobromyl, history photos' }
+      { hid: 'description', name: 'description', content: 'Добромиль, історичні фото. Dobromyl, historical photos' },
+      { hid: 'keywords', name: 'keywords', content: 'Добромиль, історичні фото, Dobromyl, historical photos, Dobromil'},
+      { hid: "og:url", property: "og:url", content: "https://dobromyl-old-photo.herokuapp.com/"},
+      { hid: "og:title", property: "og:title", content: "Добромиль, історичні фото"},
+      { hid: "og:description", property: "og:description", content: "Добромиль, історичні фото, Dobromyl, historical photos, Dobromil"},
+      { hid: "og:image", property: "og:image", content: "https://dobromyl-old-photo.herokuapp.com/photos/general/00051.jpg"},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -32,7 +37,14 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/sitemap'
   ],
+  sitemap: {
+    hostname: 'http://dobromyl-historical-photos.herokuapp.com/',
+    exclude: [
+      '/about'
+    ],
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
