088b6ca3dbf4fc19a7a7295d5b6bd62d5d6434ee
skiffua <oleh_mushora@ukr.net>
2025-07-18 20:07:20 +0300

    Fix 404

--- error.vue
+++ error.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="page404" v-show="isImg404Loaded">
-    <img @load="isImg404Loaded=true" src="../assets/404.svg">
-    <h1 v-if="error?.statusCode === 404 && isImg404Loaded">Сторінку не знайдено </h1>
+  <div class="page404">
+    <img src="./assets/404.svg" alt="404 image">
+    <h1 v-if="error?.statusCode === 404">Сторінку не знайдено </h1>
     <h1 v-else-if="error?.statusCode !== 404">Помилка... спробуйте перезавантажитись)</h1>
+    <NuxtLink to="/"><h2>Повернутись на головну</h2></NuxtLink>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { defineProps } from 'vue';
 import { definePageMeta } from '~/.nuxt/imports';
 
 // Проп error
 defineProps<{
   error?: any;
 }>();
-
-// Реактивна змінна
-const isImg404Loaded = ref(false);
 
 // Визначення layout
 definePageMeta({
@@ -25,11 +23,14 @@ definePageMeta({
 </script>
 
 <style scoped lang="scss">
-  .page404 {
-    color: burlywood;
+.page404 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: burlywood;
 
-    img {
-      width: 150px;
-    }
+  img {
+    width: 150px;
   }
+}
 </style>
